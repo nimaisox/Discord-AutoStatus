36a42a258e82e324808e22b30a9609460e6e9793
nimaisox <61984673+nimaisox@users.noreply.github.com>
2021-11-12 00:14:18 +0330

    Update AnimatedStatus.plugin.js

--- AnimatedStatus.plugin.js
+++ AnimatedStatus.plugin.js
@@ -3,6 +3,10 @@
  * @version 1.0.0
  * @description Animate Your Discord Status
  * @author nimaisox
+ * @authorId 631463369458843668
+ * @invite DStAkkypnm
+ * @source https://github.com/nimaisox/Discord-Fake-AFK
+ * @updateUrl https://github.com/nimaisox/Discord-Fake-AFK/blob/main/FakeAFK.plugin.js
  */
  module.exports = class AnimationStatus{
    
